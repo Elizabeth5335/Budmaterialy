8ceb240b8184ffa26838044453063251fd9eb343
Elizabeth5335 <elizabeth5335west@gmail.com>
2021-11-11 12:43:58 +0200

    header

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-$(document).ready(function() {
-  $('.menu-trigger').click(function() {
-    $('ul li').slideToggle(500);
-  });//end slide toggle
-  
-  $(window).resize(function() {   
-    if (  $(window).width() > 500 ) {     
-      $('ul li').removeAttr('style');
-     }
-  });//end resize
-});//end ready
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}

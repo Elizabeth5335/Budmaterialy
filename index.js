a2ce038d899d15fba382a52ae42cc171d3d0e4c4
Elizabeth5335 <elizabeth5335west@gmail.com>
2021-11-12 11:47:08 +0200

    header, colors

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+        center: new google.maps.LatLng(48.51, 12.9),
+        zoom: 10
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
